300fefe9cfb8f31bb169a736d0a7cfadf1e03334
itslikeroar <itslikeroar2412@gmail.com>
2016-04-03 14:18:47 -0400

    recompiled and re-created dumpfile

--- level4.d
+++ level4.d
@@ -5,8 +5,8 @@ level4.o:     file format elf32-i386
 Disassembly of section .text:
 
 00000000 <.text>:
-   0:	8d 45 20             	lea    0x20(%ebp),%eax
-   3:	89 45 00             	mov    %eax,0x0(%ebp)
-   6:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
-   b:	68 0e 8f 04 08       	push   $0x8048f0e
-  10:	c3                   	ret    
+   0:	8d 44 24 20          	lea    0x20(%esp),%eax
+   4:	89 45 00             	mov    %eax,0x0(%ebp)
+   7:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
+   c:	68 0e 8f 04 08       	push   $0x8048f0e
+  11:	c3                   	ret    
