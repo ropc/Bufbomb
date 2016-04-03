af75ae1728b82284a3635d291f6d0b1c865d983d
itslikeroar <itslikeroar2412@gmail.com>
2016-04-03 14:46:52 -0400

    recompiled

--- level4.d
+++ level4.d
@@ -5,7 +5,7 @@ level4.o:     file format elf32-i386
 Disassembly of section .text:
 
 00000000 <.text>:
-   0:	8d 44 24 20          	lea    0x20(%esp),%eax
+   0:	8d 44 24 18          	lea    0x18(%esp),%eax
    4:	89 45 00             	mov    %eax,0x0(%ebp)
    7:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
    c:	68 0e 8f 04 08       	push   $0x8048f0e
