024549ef07bf37a2c9a9d65ce8c77cc37aa4f055
itslikeroar <itslikeroar2412@gmail.com>
2016-04-03 15:31:46 -0400

    recompiled

--- level4.d
+++ level4.d
@@ -7,6 +7,7 @@ Disassembly of section .text:
 00000000 <.text>:
    0:	8d 44 24 18          	lea    0x18(%esp),%eax
    4:	89 45 00             	mov    %eax,0x0(%ebp)
-   7:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
-   c:	68 0e 8f 04 08       	push   $0x8048f0e
-  11:	c3                   	ret    
+   7:	c7 45 00 04 00 00 00 	movl   $0x4,0x0(%ebp)
+   e:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
+  13:	68 0e 8f 04 08       	push   $0x8048f0e
+  18:	c3                   	ret    
