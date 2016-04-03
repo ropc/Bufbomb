15c5b3818a4f413176f2b7d93912161bc9a4a002
itslikeroar <itslikeroar2412@gmail.com>
2016-04-03 19:29:54 -0400

    somewhere along with this... pushing return addrs works, but sets ebp equal to esp

--- level4.d
+++ level4.d
@@ -5,10 +5,10 @@ level4.o:     file format elf32-i386
 Disassembly of section .text:
 
 00000000 <.text>:
-   0:	8d 44 24 18          	lea    0x18(%esp),%eax
-   4:	89 45 00             	mov    %eax,0x0(%ebp)
-   7:	c7 40 fc ef be ad de 	movl   $0xdeadbeef,-0x4(%eax)
-   e:	c7 04 24 04 00 00 00 	movl   $0x4,(%esp)
-  15:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
-  1a:	68 0e 8f 04 08       	push   $0x8048f0e
-  1f:	c3                   	ret    
+   0:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
+   5:	8d 54 24 18          	lea    0x18(%esp),%edx
+   9:	c7 42 fc ef be ad de 	movl   $0xdeadbeef,-0x4(%edx)
+  10:	c7 04 24 04 00 00 00 	movl   $0x4,(%esp)
+  17:	89 e5                	mov    %esp,%ebp
+  19:	68 0e 8f 04 08       	push   $0x8048f0e
+  1e:	c3                   	ret    
--- level4.s
+++ level4.s
@@ -1,7 +1,10 @@
-leal	24(%esp), %eax
-movl	%eax, (%ebp)
-movl	$0xdeadbeef, -4(%eax)
-movl	$0x4, (%esp)
 movl	$0x5516f6f3, %eax
+leal	24(%esp), %edx		# edx = old ebp
+movl	$0xdeadbeef, -4(%edx)
+movl	$0x4, (%esp)
+movl	%esp, %ebp
 pushl	$0x8048f0e
 ret
+# ret means:
+#	mov 8(%ebp), %esp
+#	pop %ebp
