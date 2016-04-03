5e154fcf3479a1735b0412dc81edfb5d14bf6a42
itslikeroar <itslikeroar2412@gmail.com>
2016-04-03 16:18:57 -0400

    more compiling/decompiling

--- level4.d
+++ level4.d
@@ -7,7 +7,8 @@ Disassembly of section .text:
 00000000 <.text>:
    0:	8d 44 24 18          	lea    0x18(%esp),%eax
    4:	89 45 00             	mov    %eax,0x0(%ebp)
-   7:	c7 04 24 04 00 00 00 	movl   $0x4,(%esp)
-   e:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
-  13:	68 0e 8f 04 08       	push   $0x8048f0e
-  18:	c3                   	ret    
+   7:	c7 40 fc ef be ad de 	movl   $0xdeadbeef,-0x4(%eax)
+   e:	c7 04 24 04 00 00 00 	movl   $0x4,(%esp)
+  15:	b8 f3 f6 16 55       	mov    $0x5516f6f3,%eax
+  1a:	68 0e 8f 04 08       	push   $0x8048f0e
+  1f:	c3                   	ret    
